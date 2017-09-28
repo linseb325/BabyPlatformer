4ee6ab020af488bcd9c5952754ee2b774aea8360
linseb325 <linseb325@gmail.com>
2017-09-28 08:11:32 -0500

    Updated for class (HW 5). Added another platform and spikes for death.

--- Assets/_Scripts/BabyMovement.cs
+++ Assets/_Scripts/BabyMovement.cs
@@ -9,10 +9,12 @@ public class BabyMovement : MonoBehaviour
     public float jumpPower;
     public float maxSpeedX;
 
-
     Rigidbody2D rb;
     Animator anim;
+    // Collider2D collider;
+
     private bool isRunning;
+    private bool inMidair;
 
 
     // Use this for initialization
@@ -20,34 +22,40 @@ public class BabyMovement : MonoBehaviour
     {
         this.rb = this.GetComponent<Rigidbody2D>();
         this.anim = this.GetComponent<Animator>();
+        isRunning = false;
+        inMidair = false;
+        // this.collider = this.GetComponent<Collider2D>();
     }
 
 
     void Update()
     {
-        // Horizontal input
-        float moveHorizontal = Input.GetAxis("Horizontal");
 
         // Vertical input
         // float moveVertical = Input.GetAxis("Vertical");
 
 
-        // Move horizontally, but only if we're below max running speed.
-        if (this.rb.velocity.x < maxSpeedX)
+        // Apply horizontal force, but only if we're below max running speed.
+        if (Mathf.Abs(this.rb.velocity.x) < maxSpeedX)
         {
+			// Horizontal input
+			float moveHorizontal = Input.GetAxis("Horizontal");
+
 			Vector2 horizontalMovementForce = new Vector2(moveHorizontal * speed, 0);
             this.rb.AddForce(horizontalMovementForce);
 		}
 
 
         // Should we animate horizontal movement?
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (!isRunning)
+            if (!inMidair)
             {
+                print("Start running because key is down this frame");
                 this.anim.SetTrigger("shouldRun");
-                this.isRunning = true;
             }
+
+            this.isRunning = true;
         }
 
 
@@ -55,8 +63,15 @@ public class BabyMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(this.rb.velocity.y) < 0.00001f)
         {
             this.anim.SetTrigger("shouldJump");
+            this.inMidair = true;
             Vector2 jumpForce = new Vector2(0, this.jumpPower);
             this.rb.AddForce(jumpForce);
+        }
+
+        // Show the falling animation when we're moving downward.
+        if (inMidair && this.rb.velocity.y < 0.2f)
+        {
+            this.anim.SetTrigger("shouldFall");
         }
 
 
@@ -64,7 +79,12 @@ public class BabyMovement : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
             this.isRunning = false;
-            this.anim.SetTrigger("shouldIdle");
+            // Show the idle animation if we're not in midair.
+            if (!inMidair)
+            {
+                print("Idle because key is up");
+                this.anim.SetTrigger("shouldIdle");
+            }
             this.rb.velocity = new Vector2(0f, this.rb.velocity.y);
             //this.rb.angularVelocity = 0f;
         }
@@ -74,8 +94,7 @@ public class BabyMovement : MonoBehaviour
 
     void LateUpdate()
     {
-
-
+        
     }
 
 
@@ -83,4 +102,34 @@ public class BabyMovement : MonoBehaviour
     {
 
     }
+
+    // What happens when the baby hits something?
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision!");
+
+        // Hitting the ground
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            this.inMidair = false;
+
+            // Should we be running when we land, or idle?
+            if (isRunning)
+            {
+                print("Running because we hit the ground and isRunning == true");
+                this.anim.SetTrigger("shouldRun");
+            }
+            else
+            {
+                print("Idle because we hit the ground and isRunning == false");
+                this.anim.SetTrigger("shouldIdle");
+            }
+        }
+        else if (collision.gameObject.CompareTag("spikes"))
+        {
+            this.gameObject.SetActive(false);
+        }
+
+    }
+
 }
